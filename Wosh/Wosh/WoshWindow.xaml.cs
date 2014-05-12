bcf30806ba7d9f6705624e7ad3a21972fc9a2369
Braydon Hall <toonlupin@gmail.com>
2014-05-12 17:12:50 +1000

    Trying to fix .gitignore

--- Wosh/Wosh/WoshWindow.xaml.cs
+++ Wosh/Wosh/WoshWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using NUnit.Framework;
 
 namespace Wosh
 {
@@ -79,17 +78,6 @@ namespace Wosh
         {
             Canvas = new Canvas();
             DrawScreen(sender, e);
-        }
-    }
-
-    [TestFixture]
-    public class WoshTest
-    {
-        [Test]
-        public static void play() {
-            int o = 1;
-            int e = 0;
-            Console.WriteLine("Error: {0}", o/e);
         }
     }
 }
