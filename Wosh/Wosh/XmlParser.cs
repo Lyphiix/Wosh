115bc9309a8b0165fa70172d9369192304ec852a
Mrwerdo <mrwerdo331@me.com>
2014-05-13 09:35:17 +1000

    Updated XmlParser.
    Added code to connect to the server, along with changed some method
    names to comply with c# naming conventions.

--- Wosh/Wosh/XmlParser.cs
+++ Wosh/Wosh/XmlParser.cs
@@ -7,12 +7,21 @@ using NUnit.Framework;
 
 namespace Wosh
 {
+
+    /*
+     * 
+     * String content = new System.Net.WebClient().DownloadString(@"http://augo/go/cctray.xml");
+     * List<MetaData> data = XmlParser.ParseString(content);
+     * foreach (MetaData d in data) {
+     *      // Do Something Here
+     * }
+     * */
+
     public class XmlParser
     {
-        public static List<MetaData> parseFile(String filePath)
+        public static List<MetaData> ParseString(String input)
         {
-            string x = System.IO.File.ReadAllText(filePath);
-            XmlReader reader = XmlReader.Create(new System.IO.StringReader(x));
+            XmlReader reader = XmlReader.Create(new System.IO.StringReader(input));
             List<MetaData> list = new List<MetaData>();
 
             reader.ReadToFollowing("Project");
@@ -21,12 +30,12 @@ namespace Wosh
             {
                 MetaData data = new MetaData();
 
-                if (reader.MoveToAttribute("name")) data.name = reader.Value;
-                if (reader.MoveToAttribute("activity")) data.activity = reader.Value;
-                if (reader.MoveToAttribute("lastBuildStatus")) data.lastBuildStatus = reader.Value;
-                if (reader.MoveToAttribute("lastBuildLabel")) data.lastBuildLabel = reader.Value;
-                if (reader.MoveToAttribute("lastBuildTime")) data.lastBuildTime = reader.Value;
-                if (reader.MoveToAttribute("webUrl")) data.webUrl = reader.Value;
+                if (reader.MoveToAttribute("name")) data.Name = reader.Value;
+                if (reader.MoveToAttribute("activity")) data.Activity = reader.Value;
+                if (reader.MoveToAttribute("lastBuildStatus")) data.LastBuildStatus = reader.Value;
+                if (reader.MoveToAttribute("lastBuildLabel")) data.LastBuildLabel = reader.Value;
+                if (reader.MoveToAttribute("lastBuildTime")) data.LastBuildTime = reader.Value;
+                if (reader.MoveToAttribute("webUrl")) data.WebUrl = reader.Value;
 
                 list.Add(data);
                 reader.ReadToFollowing("Project");
@@ -38,21 +47,21 @@ namespace Wosh
 
     public class MetaData
     {
-        public String name;
-        public String activity;
-        public String lastBuildStatus;
-        public String lastBuildLabel;
-        public String lastBuildTime;
-        public String webUrl;
+        public String Name;
+        public String Activity;
+        public String LastBuildStatus;
+        public String LastBuildLabel;
+        public String LastBuildTime;
+        public String WebUrl;
 
         public MetaData()
         {
-            name = "(null)";
-            activity = "(null)";
-            lastBuildLabel = "(null)";
-            lastBuildStatus = "(null)";
-            lastBuildTime = "(null)";
-            webUrl = "(null)";
+            Name = "(null)";
+            Activity = "(null)";
+            LastBuildLabel = "(null)";
+            LastBuildStatus = "(null)";
+            LastBuildTime = "(null)";
+            WebUrl = "(null)";
         }
     }
 
