9fac28a7b22b4b0c64f06b539f41ca4bff75e6e9
Braydon Hall <toonlupin@gmail.com>
2014-05-13 12:29:12 +1000

    Added a dummy list

--- Wosh/Wosh/WoshWindow.xaml.cs
+++ Wosh/Wosh/WoshWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -10,11 +11,16 @@ namespace Wosh
     public partial class WoshWindow : Window
     {
         private readonly Canvas _canvas;
-        
+
         public int MaxColumns;
         public int MaxRows;
 
         //public IXmlParser TheParser;
+
+        /// <summary>
+        /// Dummy list - Will be removed
+        /// </summary>
+        public List<MetaData> MetaDatas;
 
         public WoshWindow()
         {
@@ -22,40 +28,48 @@ namespace Wosh
             _canvas = new Canvas();
             MaxColumns = 2;
             MaxRows = 10;
-            // Content Manger.
-            // mager(this);
+
+            // Dummy List
+            MetaDatas = new List<MetaData>();
+
+            for (var i = 0; i < 20; i++)
+            {
+                var meta = new MetaData
+                {
+                    Name = "Project",
+                    Activity = "Sleeping",
+                    LastBuildLabel = "(null)",
+                    LastBuildStatus = "Success",
+                    LastBuildTime = "(null)",
+                    WebUrl = "http://www.this-is-stupid.com"
+                };
+                if (i == 1 || i == 17)
+                {
+                    meta.Activity = "Building";
+                }
+                else if (i == 5 || i == 10)
+                {
+                    meta.LastBuildStatus = "Failure";
+                }
+                MetaDatas.Add(meta);
+            }
         }
 
         protected override void OnRender(DrawingContext drawingContext)
         {
-            DrawScreen();
+            DrawScreen(MetaDatas);
         }
 
-        //private void DrawScreen(List<MetaData> metaDatas)
-        private void DrawScreen()
+        private void DrawScreen(List<MetaData> metaDatas)
         {
+            var metaArray = metaDatas.ToArray();
             var counter = 0;
             for (var i = 0; i < MaxColumns; i++)
             {
                 for (var j = 0; j < MaxRows; j++)
                 {
-                    var meta = new MetaData
-                    {
-                        Name = "Project",
-                        Activity = "Sleeping",
-                        LastBuildLabel = "(null)",
-                        LastBuildStatus = "Success",
-                        LastBuildTime = "(null)",
-                        WebUrl = "http://www.this-is-stupid.com"
-                    };
-                    if (j == 1)
-                    {
-                        meta.Activity = "Building";
-                    } else if (j == 5)
-                    {
-                        meta.LastBuildStatus = "Failure";
-                    }
-                    DrawSegment(i, j, meta);
+                    DrawSegment(i, j, (MetaData)metaArray.GetValue(counter));
+                    counter++;
                 }
             }
         }
@@ -63,25 +77,25 @@ namespace Wosh
         private void DrawSegment(int column, int row, MetaData meta)
         {
             var rectangle = new Rectangle
-                {
-                    Width = (ActualWidth - 16)/MaxColumns,
-                    Height = (ActualHeight - 39)/MaxRows,
-                    Stroke = new SolidColorBrush(Colors.Black),
-                    StrokeThickness = 1,
-                    Fill = new SolidColorBrush(ColorForMetaData(meta))
-                };
+            {
+                Width = (ActualWidth - 16) / MaxColumns,
+                Height = (ActualHeight - 39) / MaxRows,
+                Stroke = new SolidColorBrush(Colors.Black),
+                StrokeThickness = 1,
+                Fill = new SolidColorBrush(ColorForMetaData(meta))
+            };
             Canvas.SetLeft(rectangle, column * rectangle.Width);
-            Canvas.SetTop(rectangle, row*rectangle.Height);
+            Canvas.SetTop(rectangle, row * rectangle.Height);
             _canvas.Children.Add(rectangle);
 
             var textBlock = new TextBlock
-                {
-                    Text = meta.Name,
-                    Foreground = new SolidColorBrush(Colors.Black),
-                    FontSize = rectangle.Height/2,
-                    VerticalAlignment = VerticalAlignment.Center,
-                    HorizontalAlignment = HorizontalAlignment.Center
-                };
+            {
+                Text = meta.Name,
+                Foreground = new SolidColorBrush(Colors.Black),
+                FontSize = rectangle.Height / 2,
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
             textBlock.Measure(new Size(rectangle.Width, rectangle.Height));
             textBlock.Arrange(new Rect(new Size(rectangle.Width, rectangle.Height)));
             Canvas.SetLeft(textBlock, (column * rectangle.Width) + ((rectangle.Width - textBlock.ActualWidth) / 2));
@@ -98,7 +112,7 @@ namespace Wosh
 
         private static Color ColorForMetaData(MetaData meta)
         {
-            if (!meta.Activity.Equals("Sleeping"))
+            if (meta.Activity.Equals("Building"))
             {
                 return Colors.Yellow;
             }
